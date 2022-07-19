fcc541975136fde5acfff1badd9726893c68af90
asfg465223 <ian8703031919@gmail.com>
2022-07-19 21:41:13 +0800

    router配置

---
+++ src/router/index.ts
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+const routes: Array<RouteRecordRaw> = [
+
+  // {
+  //   path: '/event/:id',
+  //   name: 'EventDetail',
+  //   component: () => import('@/views/AppEventDetail.vue')
+  // },
+  // {
+  //   path: '/spot/:id',
+  //   name: 'SpotDetail',
+  //   component: () => import('@/views/AppSpotDetail.vue')
+  // },
+  // {
+  //   path: '/food/:id',
+  //   name: 'FoodDetail',
+  //   component: () => import('@/views/AppFoodDetail.vue')
+  // },
+  // {
+  //   path: '/:pathMatch(.*)*', // router4，萬用字元寫法
+  //   redirect: '/home'
+  // }
+
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
